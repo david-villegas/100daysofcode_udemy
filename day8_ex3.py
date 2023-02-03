bb2f45dcc733995bee5518e53a662b991d379b1b
David Villegas <david.villegasg7@gmail.com>
2023-02-02 22:26:15 -0400

    day8

--- day8_ex3.py
+++ day8_ex3.py
@@ -1,22 +1,18 @@
-def encrypt(text,shift):
-    new_word = ""
-    for i in range(len(alphabet)):
-        if alphabet[i] in text:
-            print(i)
-
-
-    print(new_word)
-    print(shift)
-
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-encrypt(text,shift)
-
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text,shift):
+    new_word = ""
+    for i in text:
+        pos_1 = alphabet.index(i)
+        pos_2 = pos_1 + shift
+        new_letter = alphabet[pos_2]
+        new_word += new_letter
+    print(f'The encoded text is {new_word}')
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
     #e.g.
@@ -31,3 +27,4 @@ encrypt(text,shift)
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+encrypt(text, shift)
