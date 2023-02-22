868110c8aae166edadb09c01ae9c2fe5a1a4f573
David Villegas <david.villegasg7@gmail.com>
2023-02-21 20:28:14 -0400

    Adding black jack game day 11

--- prueba.py
+++ prueba.py
@@ -1,14 +1,17 @@
-import os
+sum_cards = [10,10,11]
 
-"""A String"""
+def calculate_score(sum_cards):
+    total_cards = sum(sum_cards)
+    if total_cards == 21:
+          return 0
+    elif total_cards > 21:
+        for i in sum_cards:
+            if i == 11:
+                sum_cards.append(1)
+                sum_cards.remove(11)
+                print(f'As found and change, {sum_cards}')
+        return total_cards
+    else:
+        return total_cards
 
-# A comment
-
-class Foo(object):
-    def __init__(self) -> None:
-        num = 42
-        print(num)
-
-    @property
-    def foo(self):
-        return 'bar'
+calculate_score(sum_cards)
